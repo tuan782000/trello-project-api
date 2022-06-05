ddf66bd650b5bb6fd9bc41f9cbee0058467fad10
tuan782000 <thaituan7820@gmail.com>
2022-06-05 15:57:59 +0700

    Config mongodb, use enviroment

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from 'express'
-import { mapOrder } from '*/ultilities/sorts.js'
+import { connectDB } from '*/config/mongodb'
+import { env } from '*/config/evirontment'
+
 
 const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+connectDB().catch(console.log)
 
 app.get('/', (req, res) => {
   res.end('<h1>Hello world tuannuyen</h1><hr/>')
 })
 
-app.listen(port, hostname, () => {
-  console.log(`Hello tuan, I'm running at ${hostname}:${port}/`)
+app.listen(env.PORT, env.HOST, () => {
+  console.log(`Hello tuan, I'm running at ${env.HOST}:${env.PORT}/`)
 })
